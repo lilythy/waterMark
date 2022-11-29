12b509d8791a21b8ab17f87061f0a6eb4a4d5ace
lilythy <1063609531@qq.com>
2022-11-29 14:34:40 +0800

    Create index.tsx

---
+++ index.tsx
@@ -0,0 +1,11 @@
+import WaterMarkContent from './WaterMarkContent';
+
+const Test = () => {
+  return (
+    <WaterMarkContent>
+      <div style={{ height: '400px' }}>test</div>
+    </WaterMarkContent>
+  );
+};
+
+export default Test;
